7580d0481d0981471fbbc9e86ca0db56e0977d0e
Ryan Hummel <112951711+ryanrasal@users.noreply.github.com>
2023-10-10 09:21:42 +0200

    finish

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,10 +2,10 @@ import pexelsheader from "../assets/pexelsheader.jpg";
 
 export default function Header() {
   return (
-    <div className=" h-screen relative flex flex-col text-center justify-center items-center ">
-      <img src={pexelsheader} alt="photo hacker" />
+    <div className="md:h-screen h-[50vh] relative flex flex-col text-center justify-center items-center ">
+      <img className="h-screen w-full" src={pexelsheader} alt="photo hacker" />
       <div className="absolute">
-        <h1 className="text-6xl font-bold mb-2">Ryan Beaujot</h1>
+        <h1 className="md:text-6xl text-3xl font-bold mb-2">Ryan Beaujot</h1>
         <p className="text-2xl">Développeur web Junior</p>
       </div>
     </div>
